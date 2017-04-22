8a0b262ae2854951af35fc31c0089df4341fe5d1
BalazsMarton <balazsmarton90.joomla@gmail.com>
2017-04-23 01:08:54 +0200

    actionmailer working with email tempalte n api3 key

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,12 @@
 class MessageMailer < ApplicationMailer
 
-  default from: "Your Mailer <info@csaladihazkivitelezes.com>"
-  default to: "Your Name <balazsmarton90@gmail.com>"
+  default from: "<info@csaladihazkivitelezes.com>"
+  default to: "<balazsmarton90@gmail.com>"
 
   def new_message(message)
     @message = message
 
-    mail subject: "Message from #{message.name}"
+    mail subject: "#{message.name}"
     mail from: "#{message.email}"
   end
 end
